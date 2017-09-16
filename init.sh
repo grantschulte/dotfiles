b24b199310e21e3373518e1685b6517a62baadb2
Grant Schulte <schulte.grant@gmail.com>
2017-09-16 18:42:24 -0400

    Adds gizmo zshell theme and init.sh scaffold

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Download and install dev tools
+
+# zshell
+# node
+# elm
+# nvm
+# rvm
+# atom
+# iterm
+# themes
+# vim plugins
+
+# APM Installs
+
+# elm-make
+# elm-linter
+# wrap-guide
+
+# Atom Configuration
+
+# Create $HOME/.gitconfig.local and prompt for user information
+# Create $HOME/.npmrc and prompt for author information and log user in
+
+# Create Symlinks
+
+# ln -s $HOME/dotfiles/gizmo.zsh-theme ~/.oh-my-zsh/themes/.gizmo.zsh-theme
